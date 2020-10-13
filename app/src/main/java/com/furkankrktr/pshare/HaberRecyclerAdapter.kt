46858e3e0f1c04e0854ef24138af30868022c750
FurkanKaraketir <69571844+FurkanKaraketir@users.noreply.github.com>
2020-10-13 11:03:04 +0300

    Gradle Update

--- app/src/main/java/com/furkankrktr/pshare/HaberRecyclerAdapter.kt
+++ app/src/main/java/com/furkankrktr/pshare/HaberRecyclerAdapter.kt
@@ -1,6 +1,5 @@
 package com.furkankrktr.pshare
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
@@ -10,14 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.google.android.gms.common.server.response.FastJsonResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.DocumentChange
-import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
-import com.google.firestore.v1.Document
 import kotlinx.android.synthetic.main.recycler_row.view.*
 
 open class HaberRecyclerAdapter(private val postList: ArrayList<Post>) :
