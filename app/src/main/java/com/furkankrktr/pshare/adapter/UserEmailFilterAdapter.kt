9d12b3a9913cef3bb1307ca7f8646b4817212ff7
FurkanKaraketir <69571844+FurkanKaraketir@users.noreply.github.com>
2020-11-04 12:52:47 +0300

    Color Update

--- app/src/main/java/com/furkankrktr/pshare/adapter/UserEmailFilterAdapter.kt
+++ app/src/main/java/com/furkankrktr/pshare/adapter/UserEmailFilterAdapter.kt
@@ -62,14 +62,14 @@ open class UserEmailFilterAdapter(private val postList: ArrayList<Post>) :
             holder.itemView.context.startActivity(intent)
         }
         if (holder.itemView.recycler_row_kullanici_yorum.text[0] == "#"[0]) {
-            holder.itemView.recycler_row_kullanici_yorum.setTextColor(Color.parseColor("#00FFFF"))
+            holder.itemView.recycler_row_kullanici_yorum.setTextColor(Color.parseColor("#00DCC7"))
         } else {
             holder.itemView.recycler_row_kullanici_yorum.setTextColor(Color.parseColor("#888888"))
         }
 
 
         if (holder.itemView.recycler_row_kullanici_email.text == guncelKullanici) {
-            holder.itemView.recycler_row_kullanici_email.setTextColor(Color.parseColor("#00FFFF"))
+            holder.itemView.recycler_row_kullanici_email.setTextColor(Color.parseColor("#00DCC7"))
         } else {
             holder.itemView.recycler_row_kullanici_email.setTextColor(Color.parseColor("#888888"))
         }
