a0f948a4125b46a520b5db825775cb90d75e4824
FurkanKaraketir <69571844+FurkanKaraketir@users.noreply.github.com>
2020-12-27 12:47:44 +0300

    Photo Zoom

--- app/src/main/java/com/furkankrktr/pshare/GorselActivity.kt
+++ app/src/main/java/com/furkankrktr/pshare/GorselActivity.kt
@@ -3,46 +3,40 @@ package com.furkankrktr.pshare
 import android.os.Bundle
 import android.view.View
 import android.view.Window
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.furkankrktr.pshare.databinding.ActivityGorselBinding
+import com.furkankrktr.pshare.model.ZoomClass
 
 
 @Suppress("DEPRECATION")
 class GorselActivity : AppCompatActivity() {
 
-    private lateinit var resimTamEkran: ImageView
-
+    private lateinit var resimTamEkran: ZoomClass
+    private var open: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         val binding = ActivityGorselBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         resimTamEkran = binding.resimTamEkran
         hideSystemUI()
         val resimUri = intent.getStringExtra("resim")
         Glide.with(this).load(resimUri).into(resimTamEkran)
 
         resimTamEkran.setOnClickListener {
-            showSystemUI()
+            if (open) {
+                hideSystemUI()
+            } else {
+                showSystemUI()
+            }
         }
 
-        registerForContextMenu(resimTamEkran)
-
-
-    }
-
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
     }
 
 
     private fun hideSystemUI() {
-
+        open = false
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
 
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -54,6 +48,7 @@ class GorselActivity : AppCompatActivity() {
 
 
     private fun showSystemUI() {
+        open = true
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
